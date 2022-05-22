ad069d6b91dda372d0d933078fafbce5526dd7df
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-05-22 17:28:36 +0900

    220522_1-2 serializers.ModelSerializer

--- app/rooms/serializers.py
+++ app/rooms/serializers.py
@@ -1,5 +1,7 @@
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
-
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length = 140)
+from .models import Room
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Room
+        fields = ("name", "price", "instant_book")
