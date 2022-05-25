31902082df23baea4d31a3e48ac994646cc8a575
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-05-26 00:54:42 +0900

    220526_2-1 Create Room part1(api_view, Response, cons of serializers.Serializer)

--- app/rooms/serializers.py
+++ app/rooms/serializers.py
@@ -3,10 +3,22 @@ from users.serializers import UserSerializer
 from .models import Room
 
 
-class RoomSerializer(serializers.ModelSerializer):
-
+class ReadRoomSerializer(serializers.ModelSerializer):
     user = UserSerializer()
-
     class Meta:
         model = Room
         exclude = ("modified",)
+
+class WriteRoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=140)
+    address = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    beds = serializers.IntegerField(default=1)
+    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
+    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
+    bedrooms = serializers.IntegerField(default=1)
+    bathrooms = serializers.IntegerField(default=1)
+    check_in = serializers.TimeField(default="00:00:00")
+    check_out = serializers.TimeField(default="00:00:00")
+    instant_book = serializers.BooleanField(default=False)
+    # http://127.0.0.1:8000/api/v1/rooms/?name=Dan,address=%E3%85%87%E3%85%87%E3%84%B9%E3%85%87%E3%85%81%E3%85%87,price=23,beds=2,lat=21.42,lng=23.52
--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,14 +1,28 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
-# generic view : page 등의 정보를 갖고 있다
-# generic view를 사용함으로써 별도의 설정없이 pagination 사용 가능
-class ListRoomsView(ListAPIView):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+# api_view가 없으면 django의 view와 다를 바 없음
+# django가 view를 처리하는 방식을 (drf로) 바꿔주려면 api_view 사용
+@api_view(["GET", "POST"])
+def rooms_view(req):
+    if req.method == "GET":
+        rooms = Room.objects.all()
+        room_serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(room_serializer)
+        # drf에서의 res는 django에서의 res와 다르다.
+        # django에서의 res는 HTTP Response, drf에서의 res는 API 등 더 다양
+    elif req.method == "POST":
+        room_serializer = WriteRoomSerializer(data = req.data)
+        if room_serializer.is_valid():
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 # RetrieveAPIView : 하나의 데이터만을 보고 싶을 때 사용하는 방법
 class SeeRoomView(RetrieveAPIView):
     queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+    serializer_class = ReadRoomSerializer
