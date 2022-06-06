4ed25ac9f322b353567daa3ae2bcc342bdd82bc0
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-06-06 15:45:06 +0900

    220606_ch2-5 upgrade validate()

--- app/rooms/serializers.py
+++ app/rooms/serializers.py
@@ -27,17 +27,25 @@ class WriteRoomSerializer(serializers.Serializer):
         return Room.objects.create(**validated_data)
 
     def validate(self, data):
-        if not self.instance:
+        # 인스턴스가 있으면 -> update -> 모든 param 수정 안 할 수 있음 -> need default
+        if self.instance:
+            check_in = data.get('check_in', self.instance.check_in)
+            check_out = data.get('check_out', self.instance.check_out)
+            beds = data.get('beds', self.instance.beds)
+        # 인스턴스가 없으면 -> create -> 모든 param 있음 -> no need default
+        else:
             check_in = data.get('check_in')
             check_out = data.get('check_out')
             beds = data.get('beds')
-            if check_in == check_out:
-                raise serializers.ValidationError('Not enough time between checkin-out')
-            elif beds < 3:
-                raise serializers.ValidationError('Your house is too small')
-            else:
-                return data
-                # 여기서 return한 data가 위의 create로 전달됨
+            
+        if check_in == check_out:
+            raise serializers.ValidationError('Not enough time between checkin-out')
+        elif beds < 3:
+            raise serializers.ValidationError('Your house is too small')
+        
+        # 위 과정에서 모두 이상없으면 data return
+        # 여기서 return한 data가 create()나 update()로 전달됨
+        return data
         
     def update(self, instance, validated_data):
         pass
