efabcd37090027ab7170cd416dc97655f585a444
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-07-17 23:35:41 +0900

    ch3-1~2 APIView-->ModelViewSet

--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,83 +1,26 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import permissions
+
 from .models import Room
 from .serializers import RoomSerializer
+from .permissions import IsOwner
 
+class RoomViewSet(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
-class OwnPagination(PageNumberPagination):
-    page_size = 20
-
-
-class RoomsView(APIView):
-    def get(self, request):
-        paginator = OwnPagination()
-        rooms = Room.objects.all()
-        results = paginator.paginate_queryset(rooms, request)
-        # request를 parsing한다는 건 paginator가 page query argument를 찾아내야 한다는 뜻
-        room_serializer = RoomSerializer(results, many=True, context={"request":request}).data
-        return paginator.get_paginated_response(room_serializer)
-
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        room_serializer = RoomSerializer(data = request.data)
-        print(room_serializer)
-        if room_serializer.is_valid():
-            room = room_serializer.save(user=request.user)
-            read_room_serializer = RoomSerializer(room).data
-            return Response(data = read_room_serializer,status=status.HTTP_200_OK)
+    def get_permissions(self):
+        if self.action == "list" or self.action == "retrieve":
+            permission_classes = [permissions.AllowAny]
+        elif self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
         else:
-            return Response(data = room_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            permission_classes = [IsOwner]
+        return [permission() for permission in permission_classes]
 
-
-class RoomView(APIView):
-    def get_room(self, pk):
-        try:
-            room = Room.objects.get(pk=pk)
-            return room
-        except Room.DoesNotExist:
-            return None
-
-    def get(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            serializer = RoomSerializer(room).data
-            return Response(serializer)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk):
-        room = self.get_room(pk)
-        if room is not None:
-            if room.user != request.user:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            serializer = RoomSerializer(room, data=request.data, partial=True)
-            if serializer.is_valid():
-                room = serializer.save()
-                read_room_serializer = RoomSerializer(room).data
-                return Response(read_room_serializer)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            # if, 'room'이라는 instance가 없다면 drf는 이를 create로 인식
-            # 즉, instance를 통해 Write 메소드의 종류(create, put, push,,)를 결정
-            return Response(serializer)
-        else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk):
-        # first, get room
-        room = self.get_room(pk)
-        if room is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        # second, owner validate
-        if room.user != request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        else :
-            room.delete()
-            return Response(status=status.HTTP_200_OK)
 
 @api_view(["GET"])
 def room_search(request):
--- app/users/serializers.py
+++ app/users/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'avatar', 'superhost')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'avatar', 'superhost', 'favs')
         read_only_fields = ['id', 'avatar', 'superhost']
 
     def validate_last_name(self, value):
